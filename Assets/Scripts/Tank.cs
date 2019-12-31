81a2c2aad01fd90307a581d659889a83f4da21da
Yong Ha <rladydgk0303@naver.com>
2019-12-31 15:29:45 +0900

    탱크이동 추가

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    private Vector3 targetPostion;
+    public Vector3 TargetPostion
+    {
+        set
+        {
+            this.targetPostion = value;
+        }
+
+        get
+        {
+            return this.targetPostion;
+        }
+    }
+
+    private float speed;
+    private float rotSpeed;
+
+    private void Start()
+    {
+        speed = 10.0f;
+        rotSpeed = 2.0f;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, TargetPostion) <3.0f)
+        {
+            return;
+        }
+
+        Vector3 dirRot = TargetPostion - transform.position;
+        Quaternion targetRot = Quaternion.LookRotation(dirRot);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotSpeed * Time.deltaTime);
+
+        transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+}
