3776747fb813218032bff52dd897b31649e17e18
Yong Ha <rladydgk0303@naver.com>
2019-12-31 13:46:31 +0900

    유니티 Tank 파일 올림.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                transform.position = targetPosition;
+            }
+        }
+    }
+}
