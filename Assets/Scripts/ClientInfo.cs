3e53a2d99ad459c6004824896c88a7e2cd358480
Yong Ha <rladydgk0303@naver.com>
2020-01-02 15:30:47 +0900

    방입장 추가

---
+++ Assets/Scripts/ClientInfo.cs
@@ -0,0 +1,11 @@
+﻿public class ClientInfo
+{ 
+    public string clientId;
+    public string roomId;
+
+    public ClientInfo(string clientId, string roomId)
+    {
+        this.clientId = clientId;
+        this.roomId = roomId;
+    }
+}
